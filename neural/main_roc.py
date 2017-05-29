8a680b81070708925bc0d32fde0f2f771f49af66
Aroonav Mishra <aroonav11@gmail.com>
2017-05-29 13:23:04 +0530

    Show ROC curves of all classifiers.

--- neural/main_roc.py
+++ neural/main_roc.py
@@ -1,12 +1,32 @@
+"""
+============================================
+Generate ROC Curves for all the classifiers.
+============================================
+We consider all the 31 features for classification.
+For more details about IIITBh-keystroke dataset goto: https://github.com/aroonav/IIITBh-keystroke
+For more details about CMU's dataset goto: http://www.cs.cmu.edu/~keystroke/
+These features are used to train and test NN & SVM models for
+classifying users on the basis of the timings between their keystrokes.
+
+This will generate the ROC Curves.
+"""
+# print(__doc__)
+
 import os
 import numpy as np
-# import matplotlib.pyplot as plt
 import csv
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
+from sklearn import svm
 
+
+# Fast order-preserving elimination of duplicates
+def removeDupes(seq):
+	seen = set()
+	return [ x for x in seq if x not in seen and not seen.add(x)]
 
 def convert_numerical_train(labels):
 	x = []
@@ -95,20 +115,20 @@ def load_testingData():
 
 
 # Total number of classehidden_layer_sizes.
-noOfTotalClasses = 12
+noOfTotalClasses = 5
 # Total number of vectors available for one class.
-noOfTotalVectors = 150
+noOfTotalVectors = 250
 # For training purposes for one class use first `noOfTrainingVectors` vectors.
 noOfTrainingVectors = 96
 # For testing purposes for one class use first `noOfTestingVectors` vectors.
 # noOfTestingVectors = noOfTotalVectors - noOfTrainingVectors
-noOfTestingVectors = 54
+noOfTestingVectors = 154
 # Each vector contains `noOfFeatures` features.
 noOfFeatures = 31
 # This contains the path for the dataset.
 datasetPath = os.path.normpath(os.getcwd() + os.sep + os.pardir)
 # datasetPath = datasetPath + os.sep + "DSL-StrongPasswordData.csv"
-datasetPath = datasetPath + os.sep + "OURData.csv"
+datasetPath = datasetPath + os.sep + "IIITBh-Small.csv"
 
 
 noOfInputNodes = noOfFeatures
@@ -121,6 +141,7 @@ noOfOutputNodes = noOfTotalClasses
 # y: This contains the actual classes for each training vector i.e the target.
 X,y = load_trainingData()
 test_X,test_y = load_testingData()
+classes = removeDupes(y)
 
 y = convert_numerical_train(y)
 test_y = convert_numerical_test(test_y)
@@ -132,84 +153,44 @@ test_y = np.array(test_y)
 y_binarized = label_binarize(y, classes=range(noOfTotalClasses))
 test_y_binarized = label_binarize(test_y, classes=range(noOfTotalClasses))
 
-
-
+# Neural Classifiers
 sgd_clf = MLPClassifier(hidden_layer_sizes = (noOfInputNodes, noOfHiddenNodes, noOfOutputNodes), 
 		activation = "tanh", solver = "sgd", max_iter = 1800, learning_rate = "adaptive", learning_rate_init="0.01",
 		random_state=0)
 adam_clf = MLPClassifier(hidden_layer_sizes = (noOfInputNodes, noOfHiddenNodes, noOfOutputNodes), 
 		activation = "tanh", solver = "adam", max_iter = 1000, random_state=0)
-lbfgs_clf = MLPClassifier(hidden_layer_sizes = (noOfInputNodes, noOfHiddenNodes, noOfOutputNodes), 
-		activation = "tanh", solver = "lbfgs", max_iter = 1000, random_state=0)
+# SVM Classifiers
+rbf_svc_clf = OneVsRestClassifier((svm.SVC(kernel='rbf', gamma=0.05, C=401, probability=True)))
+lin_svc_clf = OneVsRestClassifier((svm.SVC(kernel='linear', C=801, gamma=0.01, probability=True)))
 
 sgd = sgd_clf.fit(X,y_binarized)
 adam = adam_clf.fit(X,y_binarized)
-lbgs = lbfgs_clf.fit(X,y_binarized)
+lin_svc = lin_svc_clf.fit(X, y_binarized)
+rbf_svc = rbf_svc_clf.fit(X, y_binarized)
 
-for i, clf in enumerate((sgd_clf, adam_clf, lbfgs_clf)):
-	if(i==0):
-		print "Testing with SGD solver."
-	elif(i==1):
-		print "Testing with Adam solver"
-	else:
-		print "Testing with Lbfgs solver."	
-	# Pass testing data to the classifier
-	Z = clf.predict(test_X)
-	# print Z
-	similar = 0
-	for j in xrange(0, noOfTestingVectors*noOfTotalClasses):
-		# print j, Z[j], test_y[j]
-		if (Z[j]==test_y_binarized[j]).all():
-			similar+=1
-	score = (similar/float(noOfTestingVectors*noOfTotalClasses))*100
-	print "Performance of classifier:", score,"%"
-	print ""
+labels = ['Neural(SGD)', 'Neural(adam)', 'SVC(linear)', 'SVC(rbf)']
+colors = ['black', 'blue', 'darkorange', 'violet', 'yellow', 'red', 'pink', 'green', 'magenta', 'cyan', 'grey', 'brown']
 
-y_score = adam.predict_proba(test_X)
-# print y_score
-print y_score.shape
+for i, clf in enumerate((sgd, adam, lin_svc, rbf_svc)):
+	y_score = clf.predict_proba(test_X)
+	fpr = dict()
+	tpr = dict()
+	roc_auc = dict()
+	for j in range(noOfTotalClasses):
+		fpr[j], tpr[j], thresholds = roc_curve(test_y_binarized[:, j], y_score[:, j])
+		roc_auc[j] = auc(fpr[j], tpr[j])
 
-fpr = dict()
-tpr = dict()
-roc_auc = dict()
-for i in range(noOfTotalClasses):
-    fpr[i], tpr[i], thresholds = roc_curve(test_y_binarized[:, i], y_score[:, i])
-    print fpr[i], tpr[i], thresholds
-    roc_auc[i] = auc(fpr[i], tpr[i])
+	plt.figure()
+	lw = 2
+	for j,color in enumerate(colors[:noOfTotalClasses]):
+		plt.plot(fpr[j], tpr[j], color=color,
+		         lw=lw, label=classes[j]+' (area = %0.2f)' % roc_auc[j])
 
-
-plt.figure()
-lw = 2
-plt.plot(fpr[0], tpr[0], color='black',
-         lw=lw, label='subject0 (area = %0.2f)' % roc_auc[0])
-plt.plot(fpr[1], tpr[1], color='blue',
-         lw=lw, label='subject1 curve (area = %0.2f)' % roc_auc[1])
-plt.plot(fpr[2], tpr[2], color='darkorange',
-         lw=lw, label='subject2 curve (area = %0.2f)' % roc_auc[2])
-plt.plot(fpr[3], tpr[3], color='pink',
-         lw=lw, label='subject3 curve (area = %0.2f)' % roc_auc[3])
-plt.plot(fpr[4], tpr[4], color='yellow',
-         lw=lw, label='subject4 curve (area = %0.2f)' % roc_auc[4])
-plt.plot(fpr[5], tpr[5], color='red',
-         lw=lw, label='subject5 curve (area = %0.2f)' % roc_auc[5])
-plt.plot(fpr[6], tpr[6], color='violet',
-         lw=lw, label='subject6 curve (area = %0.2f)' % roc_auc[6])
-plt.plot(fpr[7], tpr[7], color='green',
-         lw=lw, label='subject7 curve (area = %0.2f)' % roc_auc[7])
-plt.plot(fpr[8], tpr[8], color='magenta',
-         lw=lw, label='subject8 curve (area = %0.2f)' % roc_auc[8])
-plt.plot(fpr[9], tpr[9], color='cyan',
-         lw=lw, label='subject9 curve (area = %0.2f)' % roc_auc[9])
-plt.plot(fpr[10], tpr[10], color='grey',
-         lw=lw, label='subject10 curve (area = %0.2f)' % roc_auc[10])
-plt.plot(fpr[11], tpr[11], color='brown',
-         lw=lw, label='subject11 curve (area = %0.2f)' % roc_auc[11])
-
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
+	plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+	plt.xlim([0.0, 1.0])
+	plt.ylim([0.0, 1.05])
+	plt.xlabel('False Positive Rate')
+	plt.ylabel('True Positive Rate')
+	plt.title('Receiver operating characteristic for '+labels[i])
+	plt.legend(loc="lower right")
 plt.show()
